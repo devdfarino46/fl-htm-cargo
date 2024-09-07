d75f1884f405f8fec2573337d09a2f0903cf19b0
Dmitry Farino <dfarino46@gmail.com>
2024-09-07 23:46:15 +0300

    скролл слайда и свайп слайдера готов

--- js/main.js
+++ js/main.js
@@ -1,21 +1,44 @@
 const pageSlider = document.querySelector('.page-slider');
+const pagination = document.querySelector('.pagination');
 
 
 if (pageSlider) {
-  
+  const slides = pageSlider.querySelectorAll('.page-slide');
 
   const slider = new Swiper(pageSlider.querySelector('.swiper'), {
     direction: 'vertical',
     slidesPerView: 1,
     pagination: {
-      el: '.pagination',
+      el: pagination,
       clickable: true
-    },
-    allowSlideNext: false,
-    allowSlidePrev: false
+    }
   });
 
-  slider.on('touchMove', () => {
-    console.log('sliderMove');
-  })
+  slider.on('slideChange', () => {
+    /**
+     * @type {HTMLElement}
+     */
+    const activeSlide = slider.slides[slider.activeIndex];
+    /**
+     * @type {HTMLElement}
+     */
+    const prevSlide = slider.slides[slider.previousIndex];
+
+    if (activeSlide.scrollHeight > window.innerHeight) {
+      slider.allowTouchMove = false;
+
+      activeSlide.addEventListener('scroll', () => {
+        if (
+          activeSlide.scrollTop === 0 ||
+          activeSlide.scrollTop + activeSlide.clientHeight >= activeSlide.scrollHeight
+        ) {
+          slider.allowTouchMove = true;
+        }
+      });
+
+      pagination.addEventListener('click', ev => {
+        prevSlide.scrollTo(0, 0);
+      });
+    }
+  });
 }
